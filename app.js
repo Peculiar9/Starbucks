6464d8f9ee53211978e83c6f2f772fe1fdbcd9e3
Peculiar Babalola <omobabson@gmail.com>
2021-08-28 14:43:54 +0100

    Responsive

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ function changeCircleColor(color) {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 }
+function toggleMenu() {
+  const menuToggle = document.querySelector(".toggle");
+  const navigation = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
